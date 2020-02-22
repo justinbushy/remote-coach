93cacefe7b76bd51ce83b13c72ba3630bedcae84
justinbushy <justinmbushphotography@gmail.com>
2020-02-22 09:57:36 -0700

    Adding drone terraform

---
+++ terraform/drone.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    profile = "default"
+    region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type" 
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+resource "aws_instance" "drone" {
+    ami = "${data.aws_ami.ubuntu.id}"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Drone"
+    }
+}
