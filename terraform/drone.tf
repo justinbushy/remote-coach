f4ded62c1060e45ad78f4e6c707c1c3b1fbed3da
justinbushy <justinmbushphotography@gmail.com>
2020-02-22 11:46:13 -0700

    Updated terraform values for drone instance

--- terraform/drone.tf
+++ terraform/drone.tf
@@ -22,6 +22,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "drone" {
     ami = "${data.aws_ami.ubuntu.id}"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-095bc4bd27480a155"]
+    subnet_id = "subnet-018fd2db4fdea3d70"
 
     tags = {
         Name = "Drone"
