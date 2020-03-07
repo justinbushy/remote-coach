64c1482e95977995bdbe71b9539a5c9dfa09adec
justinbushy <justinmbushphotography@gmail.com>
2020-03-07 10:35:08 -0700

    use postgres for django, update compose file

--- remotecoach/remotecoach/settings.py
+++ remotecoach/remotecoach/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'remotecoach.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['PGDATABASE'],
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASSWORD'],
+        'HOST': os.environ['PGHOST'],
+        'PORT': os.environ['PGPORT'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
