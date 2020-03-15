eb06378c854ccb8554959732820c4c678455376f
justinbushy <justinmbushphotography@gmail.com>
2020-03-15 09:42:48 -0600

    Add user creation test

--- remotecoach/remotecoach/user_core/tests.py
+++ remotecoach/remotecoach/user_core/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class UserTest(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user('testuser', 'test@test.com', 'testpassword')
+        self.create_url = reverse('account-create')
+
+    def test_create_user(self):
+        data = {
+            'username': 'newtestuser',
+            'email': 'new@test.com',
+            'password': 'newpassword'
+        }
+
+        response = self.client.post(self.create_url, data, format='json')
+
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+        self.assertFalse('password' in response.data)
