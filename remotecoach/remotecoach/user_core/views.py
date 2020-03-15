33229b7b2ecc3cb7a6b3b0c68a87d03f10992062
justinbushy <justinmbushphotography@gmail.com>
2020-03-15 09:25:32 -0600

    added user creation endpoint

--- remotecoach/remotecoach/user_core/views.py
+++ remotecoach/remotecoach/user_core/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from remotecoach.user_core.serializers import UserSerializer
+from rest_framework import status
 
 class HelloView(APIView):
     permission_classes = (IsAuthenticated,)
@@ -8,3 +10,15 @@ class HelloView(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+class UserCreate(APIView):
+    """
+    Create User
+    """
+
+    def post(self, request, formats='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
